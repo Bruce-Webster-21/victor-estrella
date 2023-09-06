8cdee009aa4eaaf8d4673cd40ce8c2e378453cca
Bruce Webster <brucewebster222@gmail.com>
2023-09-06 15:11:50 -0400

    Added no scroll when mobile nav menu is open

--- hamburger.js
+++ hamburger.js
@@ -1,7 +1,9 @@
 const hamburger = document.getElementById("hamburger-menu");
 const navMenu = document.getElementById('nav-menu-mobile')
+const body = document.getElementById('body')
   
 hamburger.addEventListener("click", function() {
   this.classList.toggle("open");
   navMenu.classList.toggle("translate")
+  body.classList.toggle("no-scroll")
 });
