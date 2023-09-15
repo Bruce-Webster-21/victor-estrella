dee45218f977b48166646c22a6feda9d1b65921d
Bruce Webster <brucewebster222@gmail.com>
2023-09-15 14:05:00 -0400

    Created contact sidebar

--- javascript/contactSidebar.js
+++ javascript/contactSidebar.js
@@ -1,6 +1,21 @@
 const contactBtn = document.getElementById("contact-btn")
+const contactBtn1 = document.getElementById("contact-btn-1")
+const contactBtn2 = document.getElementById("contact-btn-2")
 const contactSidebar = document.getElementById("contact-sidebar")
 
-contactBtn.addEventListener("click", function() {
-  contactSidebar.classList.toggle('show-contact-sidebar')
+const contactBtns = [contactBtn, contactBtn1, contactBtn2]
+
+contactBtns.forEach((btn) => {
+  btn.addEventListener("click", function() {
+    contactSidebar.style.translate = "100%"
+  })
 })
+
+let prevScrollpos = window.scrollY;
+window.onscroll = () => {
+  let currentScrollPos = window.scrollY;
+  if (prevScrollpos != currentScrollPos) {
+    contactSidebar.style.translate = "0%";
+  }
+  prevScrollpos = currentScrollPos;
+}
