9a30bd7e49603fc0bc2d889caaf809f5ff66ca85
Bruce Webster <brucewebster222@gmail.com>
2023-10-19 16:51:28 -0400

    Trying out javascrpt message sending

--- php/upload.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Moves uploaded file to a directory
-for ($i = 0; $i < 5; $i++) {
-  $inputName = "inpFile" . $i;
-  $targetPath = "uploads/" . basename($_FILES[$inputName]["name"]);
-  move_uploaded_file($_FILES[$inputName]["tmp_name"], $targetPath);
-}
